76b708f298d02495c053eab334cc379c5b06ae84
Gardar Benediktsson <gardarben@hotmail.com>
2020-10-21 21:22:29 -0400

    Added the BlockNrVC class. Added 2 more labels into the ShowCircleVC

---
+++ Mental Health Application/BlockNrViewController.swift
@@ -0,0 +1,21 @@
+//
+//  BlockNrViewController.swift
+//  Mental Health Application
+//
+//  Created by Garðar Benediktsson on 10/21/20.
+//  Copyright © 2020 Gardar Benediktsson. All rights reserved.
+//
+
+import UIKit
+
+class BlockNrViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+
+}
--- Mental Health Application/ShowCircleViewController.swift
+++ Mental Health Application/ShowCircleViewController.swift
@@ -13,12 +13,21 @@ class ShowCircleViewController: UIViewController {
     
     @IBOutlet weak var name1: UILabel!
     
+    @IBOutlet weak var name2: UILabel!
+    
+    @IBOutlet weak var name3: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let number = Int.random(in: 0..<10)
-        name1.text = String(number)
+        let number1 = Int.random(in: 0..<10)
+        name1.text = String(number1)
+        
+        let number2 = Int.random(in: 0..<10)
+        name2.text = String(number2)
+        
+        let number3 = Int.random(in: 0..<10)
+        name3.text = String(number3)
     }
     
    
