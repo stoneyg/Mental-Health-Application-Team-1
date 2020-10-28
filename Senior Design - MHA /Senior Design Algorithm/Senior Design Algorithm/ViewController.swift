405d5a190e35cd8ebd27f075849049725c39147d
stoneyg <stoneyg@my.erau.edu>
2020-10-28 14:55:49 -0400

    Updating Algorithm

--- Senior Design - MHA /Senior Design Algorithm/Senior Design Algorithm/ViewController.swift
+++ Senior Design - MHA /Senior Design Algorithm/Senior Design Algorithm/ViewController.swift
@@ -14,9 +14,11 @@ import Foundation
 class ViewController: UIViewController {
 
     var algorithm:[Dictionary<String,AnyObject>] = Array()
+    var trustCalculation:[Double] = []
     var dct = Dictionary<String,AnyObject>()
     var contactLogData:[[String:String]] = []
     var contactLogTitles:[String] = []
+    var trust:Double = 0.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,11 +29,10 @@ class ViewController: UIViewController {
         
         //printCSVData()
         
-        var data = readDataFromCSV(fileName: "Contact Log - MHA", fileType: ".csv")
+        var data = readDataFromCSV(fileName: "MHA - Contact Log", fileType: "csv")
         data = cleanRows(file: data!)
         let csvRows = csv(data: data!)
-        var name1 = "Simone Stoney"
-        var name2 = "Laurie Stoney"
+        
         
         for rows in csvRows {
            
@@ -39,74 +40,112 @@ class ViewController: UIViewController {
             
             let name = rows[0]
             let number = rows[1]
-            let formOfCom:String = [2]
+            let formOfCom:String = rows[2]
             var formOfComNum1 = 0.0
             var formOfComNum2 = 0.0
             var formOfComNum3 = 0.0
-            let duration = rows[3]
+            /*let duration = rows[3]
             var durationNum = 0.0
             let impact = rows[4]
-            var impactNum = 0.0
+            var impactNum:Double = Double(impact) ?? 0.0
             let experience = rows[5]
-            var experienceNum = 0.0
+            var experienceNum:Double = Double(experience) ?? 0
             let time = rows[6]
-            var timeNum = 0.0
+            var timeNum = 0.0*/
             var formOfComCall = 0.0
             var formOfComFT = 0.0
             var formOfComTxt = 0.0
             var formOfComTotal = 0.0
+            
+            
+            
+           /*( if impact.contains("Pos"){
+                impactNum = 1
+            }else {
+                impactNum = -1
+            }*/
+            
+            if formOfCom.contains("Call"){
+                (formOfComNum1 = formOfComNum1)
+                formOfComCall += 1
+            } else if formOfCom.contains("Facetime"){
+                (formOfComNum2 = formOfComNum2)
+                formOfComFT += 1
+            }else {
+                (formOfComNum3 = formOfComNum3)
+                formOfComTxt += 1
+            }
+            
+            trust = formOfComNum1+formOfComNum2+formOfComNum3
             
             formOfComTotal = formOfComFT + formOfComCall + formOfComCall
             formOfComNum1 = formOfComCall/formOfComTotal
             formOfComNum2 = formOfComFT/formOfComTotal
             formOfComNum3 = formOfComTxt/formOfComTotal
             
-            if impact.contains("Pos"){
-                impactNum = 1
+            trustCalculation.append(trust)
+            
+            
+            
+            if name.contains("Contact Name"){
+                break
             }else {
-                impactNum = -1
-            }
-            
-            if formOfCom.contains("Call"){
-                formOfComNum1 = formOfComNum1
-                formOfComCall += 1
-            } else if formOfCom.contains("Facetime"){
-                (formOfComNum2 = formOfComNum2)
-                formOfComFT += 1
-            }else {
-                formOfComNum3 = formOfComNum3
-                formOfComTxt += 1
-            }
-            
-            
-           
-            if rows == dct["Name"] as! [String]{
-                
-                let trust =
-                
                 dct.updateValue(rows as AnyObject, forKey: "Name")
+                dct.updateValue(rows as AnyObject, forKey: "Trust")
+                algorithm.append(dct)
             }
-           
             
-            dct.updateValue(rows as AnyObject, forKey: "Name")
-            dct.updateValue(rows as AnyObject, forKey: "")
+           /*var nameVal = dct["Name"]
             
+            if name == nameVal as! String{
+                
+                dct.updateValue(rows as AnyObject, forKey: "Trust")
+                algorithm.append(dct)
+                
+            }else {
             
-            
+                
+            }*/
         }
+        
+        
+        createCSV(from: algorithm)
+        trustCalculation.sort()
+        
+        
+        
+        
+       /* var algorithmData = readDataFromCSV(fileName: "Algorithm", fileType: "csv")
+        algorithmData = cleanRows(file: algorithmData!)
+        let algorithmRows = csv(data: algorithmData ?? "Nothing here")*/
+        
+        /*for algRows in algorithmRows {
+            trustCalculation.append(Double(algRows[1])!)
+           
+        }*/
+        
+       // trustCalculation.sort()
+        
+        
+        
+        
+        
+        
         
     }
     
     @IBOutlet weak var textView: UITextView!
     
+   
+    
     func createCSV(from recArray:[Dictionary<String,AnyObject>]) {
         
-        var CSVString = "\("Contact Name"), \("Form and Duration of Communication"), \("Impact and Experience"), \("Duration of Last Communication")"
+        var CSVString = "\("Contact Name"), \("Trust Value")"
         
         
         for dct in recArray {
             
-            CSVString = CSVString.appending("\(String(describing: dct["Name"]!)), \(String(describing:dct["FormOfCom"]!)), \(String(describing: dct ["Impression"])), \(String(describing: dct["Time"]!))\n")
+            CSVString = CSVString.appending("\(String(describing: dct["Name"]!)), \(String(describing:dct["Trust"]!))\n")
             
         }
         
@@ -147,7 +186,7 @@ class ViewController: UIViewController {
         
         for row in rows {
             
-            let colums = row.components(separatedBy: ";")
+            let colums = row.components(separatedBy: ",")
             result.append(colums)
         }
         return result
